f07f10ae098a94a3933f9a21ee039833cb68aea1
aopontann <aopontan0416@gmail.com>
2023-04-25 00:49:50 +0000

    vpc, vm_instance を追加 チュートリアルと同じ設定

--- main.tf
+++ main.tf
@@ -29,3 +29,26 @@ module "project_services" {
   disable_services_on_destroy = false
   disable_dependent_services  = false
 }
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+
+  allow_stopping_for_update = true
+}
