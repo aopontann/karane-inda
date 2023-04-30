01c1ef1fedb42ae30bfe6679e3fd3f092bf9f308
aopontann <aopontan0416@gmail.com>
2023-04-30 16:08:58 +0000

    バージョンをv0.1.1に固定

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ wget -q https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.20.3.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 source ~/.bashrc
-go install github.com/aopontann/karane-inda@latest
+go install github.com/aopontann/karane-inda@v0.1.1
 echo -e 'OPENAI_API_KEY=<API_KEY> \nDISCORD_TOKEN=<TOKEN>' >> .env
 go/bin/karane-inda
