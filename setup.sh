a83f0b1b7c46cb93e0f5ef7d1bc3993b9c8c0766
aopontann <aopontan0416@gmail.com>
2023-04-30 17:02:40 +0000

    GOPATH, GOCACHEを追加

--- setup.sh
+++ setup.sh
@@ -4,7 +4,9 @@ apt-get install -y wget
 wget -q https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.20.3.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+echo 'export GOPATH=/go' >> ~/.bashrc
+echo 'export GOCACHE=/root/.cache/go-build' >> ~/.bashrc
 source ~/.bashrc
 go install github.com/aopontann/karane-inda@v0.1.1
 echo -e 'OPENAI_API_KEY=<API_KEY> \nDISCORD_TOKEN=<TOKEN>' >> .env
-go/bin/karane-inda
+/go/bin/karane-inda
