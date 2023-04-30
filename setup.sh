697b032f26a13f273d7632344bf8d798c500e57f
aopontann <aopontan0416@gmail.com>
2023-04-30 15:51:06 +0000

    go install で取得したバイナリから起動するように変更

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 apt-get update
 apt-get install -y wget
-wget https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
+wget -q https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.20.3.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile
-EOF
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+source ~/.bashrc
+go install github.com/aopontann/karane-inda@latest
+echo -e 'OPENAI_API_KEY=<API_KEY> \nDISCORD_TOKEN=<TOKEN>' >> .env
+go/bin/karane-inda
